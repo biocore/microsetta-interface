9b29a3708b5686a52b31b572992d434904373e04
Daniel McDonald <danielmcdonald@ucsd.edu>
2020-06-22 17:16:19 -0700

    Initial files, versioning, etc

---
+++ setup.py
@@ -0,0 +1,25 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2019-, The Microsetta Initiative development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+import versioneer
+
+setup(
+    name="microsetta-interface",
+    author="Daniel McDonald",
+    author_email="danielmcdonald@ucsd.edu",
+    packages=find_packages(),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url="https://github.com/biocore/microsetta-interface",
+    description="The Microsetta participant interface",
+    license='BSD-3-Clause',
+    package_data={'microsetta_interface':
+                  []},
+)

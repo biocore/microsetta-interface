11c5775f045817c6756a8b917c9e83e09d04e68e
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-05-28 12:17:35 -0700

    Compile on install

--- setup.py
+++ setup.py
@@ -7,8 +7,25 @@
 # ----------------------------------------------------------------------------
 
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 import versioneer
+
+# adapting https://stackoverflow.com/a/41120180/19741
+class InstallWithCompile(install):
+    def run(self):
+        from babel.messages.frontend import compile_catalog
+        compiler = compile_catalog(self.distribution)
+        option_dict = self.distribution.get_option_dict('compile_catalog')
+        compiler.domain = [option_dict['domain'][1]]
+        compiler.directory = option_dict['directory'][1]
+        compiler.run()
+        super().run()
+
+
+command_classes = versioneer.get_cmdclass()
+command_classes['install'] = InstallWithCompile
+
 
 setup(
     name="microsetta-interface",
@@ -16,53 +33,56 @@ setup(
     author_email="danielmcdonald@ucsd.edu",
     packages=find_packages(),
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    cmdclass=command_classes,
     url="https://github.com/biocore/microsetta-interface",
     description="The Microsetta participant interface",
     license='BSD-3-Clause',
-    package_data={'microsetta_interface': [
-        'routes.yaml',
-        'server_config.json',
-        'templates/*.*',
-        'static/*',
-        'static/css/*',
-        'static/img/*',
-        'static/js/*',
-        'static/vendor/js/*',
-        'static/vendor/images/*',
-        'static/vendor/css/*',
-        'static/vendor/js/jquery.form-4.2.2/*',
-        'static/vendor/jquery-ui-1.12.1/*',
-        'static/vendor/jquery-ui-1.12.1/images/*',
-        'static/vendor/popper-1.11.0/*',
-        'static/vendor/bootstrap-4.4.1-dist/js/*',
-        'static/vendor/bootstrap-4.4.1-dist/css/*',
-        'static/vendor/bootstrap-datetimepicker-4.14.30/*',
-        'static/vendor/vue-form-generator-2.3.4/*',
-        'static/vendor/bootstrap-3.3.7-dist/js/*',
-        'static/vendor/bootstrap-3.3.7-dist/css/*',
-        'static/vendor/bootstrap-3.3.7-dist/fonts/*',
-        'static/vendor/bootstrap-3.3.7-dist/fonts/*',
-        'static/vendor/font-awesome-4.7.0/css/*',
-        'static/vendor/font-awesome-4.7.0/less/*',
-        'static/vendor/font-awesome-4.7.0/fonts/*',
-        'static/vendor/font-awesome-4.7.0/scss/*',
-        'static/vendor/DataTables/*',
-        'static/vendor/DataTables/Buttons-1.6.2/js/*',
-        'static/vendor/DataTables/Buttons-1.6.2/css/*',
-        'static/vendor/DataTables/PercentageBars-1.10.21/js/*',
-        'static/vendor/DataTables/PercentageBars-1.10.21/css/*',
-        'static/vendor/emperor/*',
-        'static/vendor/emperor/css/*',
-        'static/vendor/emperor/img/*',
-        'static/vendor/emperor/js/*',
-        'static/vendor/emperor/templates/*',
-        'static/vendor/emperor/vendor/*',
-        'static/vendor/emperor/vendor/css/*',
-        'static/vendor/emperor/vendor/css/font/*',
-        'static/vendor/emperor/vendor/css/images/*',
-        'static/vendor/emperor/vendor/js/*',
-        'static/vendor/emperor/vendor/js/three.js-plugins/*',
-        'static/vendor/emperor/vendor/',
-        'authrocket.pubkey']},
+    package_data={
+        '': ['translations/*/*/*.mo',
+             'translations/*/*/*.po'],
+        'microsetta_interface': [
+            'routes.yaml',
+            'server_config.json',
+            'templates/*.*',
+            'static/*',
+            'static/css/*',
+            'static/img/*',
+            'static/js/*',
+            'static/vendor/js/*',
+            'static/vendor/images/*',
+            'static/vendor/css/*',
+            'static/vendor/js/jquery.form-4.2.2/*',
+            'static/vendor/jquery-ui-1.12.1/*',
+            'static/vendor/jquery-ui-1.12.1/images/*',
+            'static/vendor/popper-1.11.0/*',
+            'static/vendor/bootstrap-4.4.1-dist/js/*',
+            'static/vendor/bootstrap-4.4.1-dist/css/*',
+            'static/vendor/bootstrap-datetimepicker-4.14.30/*',
+            'static/vendor/vue-form-generator-2.3.4/*',
+            'static/vendor/bootstrap-3.3.7-dist/js/*',
+            'static/vendor/bootstrap-3.3.7-dist/css/*',
+            'static/vendor/bootstrap-3.3.7-dist/fonts/*',
+            'static/vendor/bootstrap-3.3.7-dist/fonts/*',
+            'static/vendor/font-awesome-4.7.0/css/*',
+            'static/vendor/font-awesome-4.7.0/less/*',
+            'static/vendor/font-awesome-4.7.0/fonts/*',
+            'static/vendor/font-awesome-4.7.0/scss/*',
+            'static/vendor/DataTables/*',
+            'static/vendor/DataTables/Buttons-1.6.2/js/*',
+            'static/vendor/DataTables/Buttons-1.6.2/css/*',
+            'static/vendor/DataTables/PercentageBars-1.10.21/js/*',
+            'static/vendor/DataTables/PercentageBars-1.10.21/css/*',
+            'static/vendor/emperor/*',
+            'static/vendor/emperor/css/*',
+            'static/vendor/emperor/img/*',
+            'static/vendor/emperor/js/*',
+            'static/vendor/emperor/templates/*',
+            'static/vendor/emperor/vendor/*',
+            'static/vendor/emperor/vendor/css/*',
+            'static/vendor/emperor/vendor/css/font/*',
+            'static/vendor/emperor/vendor/css/images/*',
+            'static/vendor/emperor/vendor/js/*',
+            'static/vendor/emperor/vendor/js/three.js-plugins/*',
+            'static/vendor/emperor/vendor/',
+            'authrocket.pubkey']},
 )

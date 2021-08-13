85251da5e55e8c8da25f384f5ff64a37adee56d4
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-08-13 10:12:23 -0700

    BUG: fallback to default on best_match

--- microsetta_interface/implementation.py
+++ microsetta_interface/implementation.py
@@ -558,7 +558,8 @@ def get_create_account():
     email, _ = _parse_jwt(session[TOKEN_KEY_NAME])
 
     browser_lang = request.accept_languages.best_match(
-        [LANGUAGES[lang].value for lang in LANGUAGES], default=LANGUAGES[EN_US_KEY].value)
+        [LANGUAGES[lang].value for lang in LANGUAGES],
+        default=LANGUAGES[EN_US_KEY].value)
     # TODO:  Need to support other countries
     #  and not default to US and California
     default_account_values = {
@@ -1429,10 +1430,11 @@ def post_generate_activation(body):
                         code_map[e] = diagnostics[0]['code']
 
             csv_output = "EMAIL,ACTIVATION_CODE\n"
-            for email,code in code_map.items():
+            for email, code in code_map.items():
                 csv_output += email + "," + code + "\n"
             response = make_response(csv_output)
-            response.headers["Content-Disposition"] = "attachment; filename=activation_codes.csv"
+            response.headers["Content-Disposition"] = \
+                "attachment; filename=activation_codes.csv"
             response.headers["Content-Type"] = "text/csv"
             return response
     else:
@@ -1512,7 +1514,8 @@ def session_locale():
 
     # TODO: We update this as we add support for new languages
     return request.accept_languages.best_match(
-        [LANGUAGES[lang].value for lang in LANGUAGES])
+        [LANGUAGES[lang].value for lang in LANGUAGES],
+        default=LANGUAGES[EN_US_KEY].value)
 
 
 class BearerAuth(AuthBase):

6b777d315dd94f0255cf761f501b4610d6ae7c57
Cassidy Symons <csymons@eng.ucsd.edu>
2023-12-09 21:13:17 -0800

    Lint

--- microsetta_interface/implementation.py
+++ microsetta_interface/implementation.py
@@ -1668,7 +1668,7 @@ def render_consent_page(account_id, source_id, form_type, sample_ids=None,
         cur_age_index = HUMAN_CONSENT_AGE_GROUPS.index(
             source_output['consent']['age_range']
         )
-    except ValueError as e:
+    except ValueError:
         # In case a source has a blank, "legacy" or faulty age range
         cur_age_index = -1
 
